f99aa508bb7d183c2f9389d44adc8eb83fdf1da6
jajeonmatholic-max <jajeon.matholic@gmail.com>
2026-05-14 19:10:15 +0900

    Force Service Worker update with prompt mode

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       manifest: {
         name: 'Matrix Todo',
         short_name: 'MatrixTodo',
