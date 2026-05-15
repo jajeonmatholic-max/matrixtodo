b0738b58c5fc3481b44b30cd4d9139d6d68eab61
jajeonmatholic-max <jajeon.matholic@gmail.com>
2026-05-15 14:07:10 +0900

    Rebuild and update docs folder with calendar tab and all-dates display features

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/matrixtodo/sw.js', { scope: '/matrixtodo/' }).then(reg => {setInterval(() => {reg.update()}, 30000)})})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/matrixtodo/sw.js', { scope: '/matrixtodo/' })})}
